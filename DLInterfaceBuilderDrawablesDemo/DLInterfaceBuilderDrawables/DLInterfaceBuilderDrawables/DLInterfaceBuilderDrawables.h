626d079135b9a2a7e74cef7b4e08b6fa9ba44f5f
David <davidlivadaru@gmail.com>
2016-07-18 21:42:37 +0300

    Add framework project and demo project.

---
+++ DLInterfaceBuilderDrawablesDemo/DLInterfaceBuilderDrawables/DLInterfaceBuilderDrawables/DLInterfaceBuilderDrawables.h
@@ -0,0 +1,19 @@
+//
+//  DLInterfaceBuilderDrawables.h
+//  DLInterfaceBuilderDrawables
+//
+//  Created by David Livadaru on 18/07/16.
+//  Copyright © 2016 Community. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLInterfaceBuilderDrawables.
+FOUNDATION_EXPORT double DLInterfaceBuilderDrawablesVersionNumber;
+
+//! Project version string for DLInterfaceBuilderDrawables.
+FOUNDATION_EXPORT const unsigned char DLInterfaceBuilderDrawablesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLInterfaceBuilderDrawables/PublicHeader.h>
+
+
